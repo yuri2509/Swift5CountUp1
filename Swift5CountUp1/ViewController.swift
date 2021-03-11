bab7ae7e66d695d1029181f84230b4416f3496d7
yuri2509 <gatx207-nicol@docomo.ne.jp>
2021-03-12 08:15:04 +0900

    数の足し引きをして結果を表示する

--- Swift5CountUp1/ViewController.swift
+++ Swift5CountUp1/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        countLabel.text = "0"
     }
 
-
+    
+    
+    @IBAction func plus(_ sender: Any) {
+        
+        //カウントアップさせたい
+        count = count + 1
+        
+        //ラベルに文字を反映したい（数字）
+        countLabel.text = String(count)
+        
+        //数が１０になったら色を黄色にする
+        
+        
+    }
+    
+    
+    
+    @IBAction func minus(_ sender: Any) {
+        
+        //カウントダウンさせたい
+        count = count - 1
+        
+        //ラベルに文字を反映したい（数字）
+        countLabel.text = String(count)
+        
+        //０になったら色を白にする
+        
+    }
+    
 }
 
