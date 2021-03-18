41abea521ff8a3db8ffb687e2623ac734253b124
yuri2509 <gatx207-nicol@docomo.ne.jp>
2021-03-18 21:40:56 +0900

    数の表示の変更

--- Swift5CountUp1/ViewController.swift
+++ Swift5CountUp1/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        countLabel.text = "0"
+        countLabel.text = String(count)
     }
 
     
